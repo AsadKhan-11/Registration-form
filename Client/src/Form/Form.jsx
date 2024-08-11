1e4ea7b498a2dd98917d969c830572efb56834a3
Asad Khan <mrasad10khan@gmail.com>
2024-08-11 20:12:00 +0500

    Minor changes

--- Client/src/Form/Form.jsx
+++ Client/src/Form/Form.jsx
@@ -6,16 +6,25 @@ function Form() {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
+  const [err, setErr] = useState();
 
   const Submit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:8000/users", { name, email, password })
+      .post("http://localhost:8000/createUser", { name, email, password })
       .then((result) => {
         console.log(result);
+        setMessage(result.data.message);
+        if (result.data.message === "Account created succesfully") {
+          setErr(false);
+        } else if (result.data.message === "Already has an account") {
+          setErr(true);
+        }
+        console.log(result.data.message);
       })
       .catch((err) => {
-        console.log(err);
+        console.log("Error:", err);
       });
   };
   return (
@@ -49,6 +58,7 @@ function Form() {
         />
       </div>
       <button type="submit">Add User</button>
+      {message && <p style={{ color: err ? "red" : "green" }}> {message} </p>}
     </form>
   );
 }
